aef1b04e15ba31119cdb916b67f49643acaa8323
Navi <pixeldevs.diego@gmail.com>
2026-04-11 17:01:37 -0600

    Add GameManager to autoload

--- Scripts/GameManager.gd
+++ Scripts/GameManager.gd
@@ -3,4 +3,9 @@ extends Node
 export var camera_path : NodePath  # drag your Camera2D in the Inspector
 
 func _ready() -> void:
-	pass
+	print("Operating System is: " + OS.get_name())
+	
+func _process(_delta) -> void:
+	if OS.get_name() == "Windows" or OS.get_name() == "X11":
+		if Input.is_action_just_released("pause"):
+			get_tree().quit()
